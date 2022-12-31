5076e3e83deb0e02746a46982809abf0b1f64f8b
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-12-31 17:20:43 +0000

    WIP

--- 7-log-scale/app.js
+++ 7-log-scale/app.js
@@ -18,6 +18,23 @@ async function draw() {
     .append('svg')
     .attr('width', dimensions.width)
     .attr('height', dimensions.height)
+
+  const circlesGroup = svg.append('g')
+
+  circlesGroup
+    .selectAll('circle') // Select all the circle elements (none yet)
+    .data(dataset) // Join the dataset with the DOM elements
+    .join('circle') // Draw circles
+    .attr('cx', dimensions.margin)
+    .attr('cy', dimensions.margin)
+    .attr('r', 6)
+
+  circlesGroup
+    .selectAll('text')
+    .data(dataset)
+    .join('text')
+    .attr('x', dimensions.margin + 15, 'y', dimensions.margin)
+    .text(nameAccessor)
 }
 
 draw()
