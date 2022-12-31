166242382e1f74e8625cf5c9476d31814f487850
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-12-31 10:56:48 +0000

    WIP

--- example-2/app.js
+++ example-2/app.js
@@ -1,5 +1,20 @@
 const data = [10, 20, 30, 40, 50]
 
-const el = d3.selectAll('li').data(data)
+const el = d3
+  .select('ul')
+  .selectAll('li')
+  .data(data)
+  .join(
+    (enter) => {
+      return enter.append('li').style('color', 'purple')
+    },
+    (update) => {
+      return update.style('color', 'green')
+    },
+    (exit) => {
+      return exit.remove()
+    }
+  )
+  .text((d) => d)
 
 console.log(el)
