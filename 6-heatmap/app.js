ec06fca3ee0af4dc8dfd9fe42715cadddeae1e39
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-12-31 15:29:08 +0000

    Add heatmap

--- 6-heatmap/app.js
+++ 6-heatmap/app.js
@@ -1,6 +1,10 @@
-async function draw(el) {
+'use strict'
+
+async function draw(el, scale) {
   // Data
   const dataset = await d3.json('data.json')
+
+  const box = 30
 
   // Dimensions
   let dimensions = {
@@ -14,6 +18,30 @@ async function draw(el) {
     .append('svg')
     .attr('width', dimensions.width)
     .attr('height', dimensions.height)
+
+  // Scales
+  let colorScale
+
+  if (scale === 'linear') {
+    colorScale = d3
+      .scaleLinear()
+      .domain(d3.extent(dataset))
+      .range(['white', 'red'])
+  }
+
+  // Rectangles
+  svg
+    .append('g')
+    .attr('transform', 'translate(2, 2)')
+    .attr('stroke', 'black')
+    .selectAll('rect')
+    .data(dataset)
+    .join('rect')
+    .attr('width', box - 3)
+    .attr('height', box - 3)
+    .attr('x', (d, i) => box * (i % 20))
+    .attr('fill', (d) => colorScale(d))
+    .attr('y', (d, i) => box * ((i / 20) | 0))
 }
 
-draw('#heatmap1')
+draw('#heatmap1', 'linear')
