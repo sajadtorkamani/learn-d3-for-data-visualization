7991d81f728ec26fb7a4c5bfbd0f5673af764db4
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-01-01 12:45:41 +0000

    Add voronoi diagram

--- 5-scatterplot/app.js
+++ 5-scatterplot/app.js
@@ -65,33 +65,6 @@ async function draw() {
     .attr('r', 5)
     .attr('fill', 'red')
     .attr('data-temp', yAccessor)
-    .on('mouseenter', function (event, datum) {
-      // Style the active dot
-      d3.select(this).attr('fill', '#120078').attr('r', 8)
-
-      // Show tooltip and position it next to the dot
-      tooltip
-        .style('display', 'block')
-        .style('top', yScale(yAccessor(datum)) - 25 + 'px')
-        .style('left', xScale(xAccessor(datum)) + 'px')
-
-      const formatter = d3.format('.2f')
-      const dateFormatter = d3.timeFormat('%B %-d, %Y')
-
-      // Set tooltip contents
-      tooltip.select('.metric-humidity span').text(formatter(xAccessor(datum)))
-      tooltip.select('.metric-temp span').text(formatter(yAccessor(datum)))
-      tooltip
-        .select('.metric-date')
-        .text(dateFormatter(datum.currently.time * 1000))
-    })
-    .on('mouseleave', function (event) {
-      // Revert styles of active dot
-      d3.select(this).attr('fill', 'red').attr('r', 5)
-
-      // Hide tooltip
-      tooltip.style('display', 'none')
-    })
 
   // Draw X axis
   const xAxis = d3
@@ -125,6 +98,61 @@ async function draw() {
     .html('Temperature &deg; F')
     .style('transform', 'rotate(270deg)')
     .style('text-anchor', 'middle')
+
+  const delaunay = d3.Delaunay.from(
+    dataset,
+    (d) => xScale(xAccessor(d)),
+    (d) => yScale(yAccessor(d))
+  )
+
+  const voronoi = delaunay.voronoi()
+  voronoi.xmax = dimensions.containerWidth
+  voronoi.ymax = dimensions.containerHeight
+
+  console.log(voronoi)
+
+  container
+    .append('g')
+    .selectAll('path')
+    .data(dataset)
+    .join('path')
+    // .attr('stroke', 'black')
+    .attr('fill', 'transparent')
+    .attr('d', (d, i) => voronoi.renderCell(i))
+    .on('mouseenter', function (event, datum) {
+      // Style the active dot
+      container
+        .append('circle')
+        .classed('dot-hovered', true)
+        .attr('fill', '#120078')
+        .attr('r', 8)
+        .attr('cx', () => xScale(xAccessor(datum)))
+        .attr('cy', () => yScale(yAccessor(datum)))
+        .style('pointer-events', 'none')
+
+      // Show tooltip and position it next to the dot
+      tooltip
+        .style('display', 'block')
+        .style('top', yScale(yAccessor(datum)) - 25 + 'px')
+        .style('left', xScale(xAccessor(datum)) + 'px')
+
+      const formatter = d3.format('.2f')
+      const dateFormatter = d3.timeFormat('%B %-d, %Y')
+
+      // Set tooltip contents
+      tooltip.select('.metric-humidity span').text(formatter(xAccessor(datum)))
+      tooltip.select('.metric-temp span').text(formatter(yAccessor(datum)))
+      tooltip
+        .select('.metric-date')
+        .text(dateFormatter(datum.currently.time * 1000))
+    })
+    .on('mouseleave', function (event) {
+      // Revert styles of active dot
+      container.select('.dot-hovered').remove()
+
+      // Hide tooltip
+      tooltip.style('display', 'none')
+    })
 }
 
 draw()
