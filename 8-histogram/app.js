01447ca12f5d90c1ba153109f85e55390fbedcca
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-01-01 10:14:43 +0000

    WIP

---
+++ 8-histogram/app.js
@@ -0,0 +1,30 @@
+async function draw() {
+  // Data
+  const dataset = await d3.json('data.json')
+
+  // Dimensions
+  let dimensions = {
+    width: 800,
+    height: 400,
+    margins: 50,
+  }
+
+  dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
+  dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
+
+  // Draw Image
+  const svg = d3
+    .select('#chart')
+    .append('svg')
+    .attr('width', dimensions.width)
+    .attr('height', dimensions.height)
+
+  const ctr = svg
+    .append('g') // <g>
+    .attr(
+      'transform',
+      `translate(${dimensions.margins}, ${dimensions.margins})`
+    )
+}
+
+draw()
