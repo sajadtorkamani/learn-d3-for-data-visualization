c4460041354f53e3de1db27b7dc0a1a8231c39dc
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-01-01 10:34:47 +0000

    WIP

--- 8-histogram/app.js
+++ 8-histogram/app.js
@@ -2,6 +2,8 @@ async function draw() {
   // Data
   const dataset = await d3.json('data.json')
 
+  const xAccessor = (d) => d.currently.humidity
+
   // Dimensions
   let dimensions = {
     width: 800,
@@ -25,6 +27,29 @@ async function draw() {
       'transform',
       `translate(${dimensions.margins}, ${dimensions.margins})`
     )
+
+  // Scales
+  const xScale = d3
+    .scaleLinear()
+    .domain(d3.extent(dataset, xAccessor))
+    .range([0, dimensions.ctrWidth])
+    .nice()
+
+  const bin = d3.bin().domain(xScale.domain()).value(xAccessor).thresholds(10)
+
+  const newDataset = bin(dataset)
+
+  console.log({ originalData: dataset, newData: newDataset })
+
+  // Draw bars
+  ctr
+    .selectAll('rect')
+    .data(dataset)
+    .join('rect')
+    .attr('width', 5)
+    .attr('height', 100)
+    .attr('x', (d) => xScale(xAccessor(d)))
+    .attr('y', 0)
 }
 
 draw()
