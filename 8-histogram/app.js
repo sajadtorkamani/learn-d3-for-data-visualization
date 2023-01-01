1b7c71456d146851879fa44ee096baf8e5ed3659
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-01-01 11:34:11 +0000

    WIP

--- 8-histogram/app.js
+++ 8-histogram/app.js
@@ -60,8 +60,19 @@ async function draw() {
     ctr
       .selectAll('rect')
       .data(newDataset)
-      .join('rect')
+      .join((enter) =>
+        enter
+          .append('rect')
+          .attr('width', (d) =>
+            d3.max([0, xScale(d.x1) - xScale(d.x0) - padding])
+          )
+          .attr('height', 0)
+          .attr('x', (d) => xScale(d.x0))
+          .attr('y', dimensions.ctrHeight)
+          .attr('fill', '#01c5c4')
+      )
       .transition()
+      .duration(3000)
       .attr('width', (d) => d3.max([0, xScale(d.x1) - xScale(d.x0) - padding]))
       .attr('height', (d) => dimensions.ctrHeight - yScale(yAccessor(d)))
       .attr('x', (d) => xScale(d.x0))
