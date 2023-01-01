84b0bf9b2f49935f1f3823339527ab3b6066ce3e
Sajad Torkamani <sajadtorkamani1@gmail.com>
2023-01-01 15:23:33 +0000

    WIP

--- 10-pie-chart/app.js
+++ 10-pie-chart/app.js
@@ -12,6 +12,8 @@ async function draw() {
   dimensions.ctrWidth = dimensions.width - dimensions.margins * 2
   dimensions.ctrHeight = dimensions.height - dimensions.margins * 2
 
+  const radius = dimensions.ctrWidth / 2
+
   // Draw Image
   const svg = d3
     .select('#chart')
@@ -27,6 +29,20 @@ async function draw() {
     )
 
   // Scales
+  const populationPie = d3.pie().value((d) => d.value)
+  const slices = populationPie(dataset)
+
+  const arc = d3.arc().outerRadius(radius).innerRadius(0)
+
+  // Draw shape
+  const arcGroup = ctr
+    .append('g')
+    .attr(
+      'transform',
+      `translate(${dimensions.ctrHeight / 2}, ${dimensions.ctrWidth / 2})`
+    )
+
+  arcGroup.selectAll('path').data(slices).join('path').attr('d', arc)
 }
 
 draw()
